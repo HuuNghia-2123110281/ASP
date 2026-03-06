d10151db27e9be39abae2e7bed729a1902631101
Nghia Nguyen <nguyenhuunghia03052004@gmail.com>
2026-03-06 10:22:30 +0700

    Day toan bo code controller va model cua Nghia

--- asp/Controllers/WeatherForecastController.cs
+++ asp/Controllers/WeatherForecastController.cs
@@ -25,7 +25,8 @@ namespace asp.Controllers
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+                StudentName = "Nguyen Huu Nghia",
             })
             .ToArray();
         }
--- asp/WeatherForecast.cs
+++ asp/WeatherForecast.cs
@@ -9,5 +9,7 @@ namespace asp
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
+        public string? StudentName { get; set; }
     }
 }
