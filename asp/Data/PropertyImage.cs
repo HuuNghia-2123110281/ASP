f5335b4871e1a7c43372d16330db6a8b427187df
Nguyen Huu Nghia <nguyenhuunghia03052004@gmail.com>
2026-04-24 08:05:08 +0700

    trang tin tức ( news )

--- asp/Data/PropertyImage.cs
+++ asp/Data/PropertyImage.cs
@@ -9,14 +9,12 @@ namespace asp.Data
         [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
 
-        // Khóa ngoại liên kết với _id của Collection BatDongSans
         [BsonRepresentation(BsonType.ObjectId)]
         public string PropertyId { get; set; } = null!;
 
         [BsonElement("ImageUrl")]
         public string ImageUrl { get; set; } = null!;
 
-        // Đánh dấu true nếu đây là ảnh bìa hiển thị ngoài trang chủ
         public bool IsMain { get; set; }
     }
 }
